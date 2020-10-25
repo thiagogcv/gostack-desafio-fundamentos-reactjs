968230ddf6bcfa8a9cd515baf9a0022bc8570f8d
Thiago Costa <thiagogcv@gmail.com>
2020-10-25 01:34:02 -0300

    Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import { Link } from 'react-router-dom';
@@ -15,9 +16,8 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
+        <Link to="/">Listagem</Link>
+        <Link to="/import">Importar</Link>
       </nav>
     </header>
   </Container>
